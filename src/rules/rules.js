6d0326fb009cf3c4fc5767ed0ab2dec8a86880c0
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-02-15 17:19:56 +0000

    chore: remove rule levels

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -1,7 +1,6 @@
 export const rules = [
   {
     category: "punctuation",
-    level: "negative",
     name: "&",
     fix: "Use 'and' instead",
     match: /&/g,
@@ -9,14 +8,12 @@ export const rules = [
   {
     category: "punctuation",
     name: "Cannot",
-    level: "warning",
     fix: "Use 'can't' instead",
     match: /\bcan not\b|\bcannot\b/g,
   },
   {
     category: "punctuation",
     name: "Do not",
-    level: "warning",
     fix: "Use 'don't' instead",
     match: /\bdo not\b/g,
   },
